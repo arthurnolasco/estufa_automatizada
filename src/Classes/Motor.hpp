4eb8a5495de10b1dd179797fae0390a068d551cc
Arthur Nolasco <129460584+arthurnolasco@users.noreply.github.com>
2025-01-22 12:58:04 -0300

    Add files via upload

---
+++ src/Classes/Motor.hpp
@@ -0,0 +1,35 @@
+#ifndef MOTOR_HPP
+#define MOTOR_HPP
+
+class Motor{
+public:
+	/**
+	* \brief Construtor do motor
+	*/
+	Motor(int step, int dir, int led);
+	
+	/**
+	 * @brief Gira o motor, caso delay seja neg, o motor para
+	 * 
+	 * @param MOTOR_DIR 
+	 * @param MOTOR_STEP 
+	 */
+	void spin();
+
+	/**
+	* @brief Atualiza o delay de acordo com as temperaturas atual e alvo,
+  * sendo delay sempre maior que 2ms
+	*
+	* @param current_temp Temperatura atual
+	* @param target_temp Temperatura alvo
+	*/
+	void update_delay(float current_temp, float target_temp);
+
+private:
+	int _step; //quantidade de passos
+	int _dir; //sentido de giro 0 = AH, 1 = H
+	int _led; 
+	float _delay = 5000; //delay entre os passos
+};
+
+#endif //MOTOR_HPP
